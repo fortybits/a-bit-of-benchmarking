85365be9cd3a6717a4d8829affebd26aae3dfbf6
Naman <namannigam12@gmail.com>
2021-01-15 23:52:29 +0530

    correct formatting

--- src/main/java/jmh/CollectorsVsStreamToList.java
+++ src/main/java/jmh/CollectorsVsStreamToList.java
@@ -25,32 +25,31 @@ public class CollectorsVsStreamToList {
     public List<Integer> viaStream() {
         return IntStream.range(1, 1000).boxed().toList();
     }
-
-    /*
-     * Benchmark                                                       Mode  Cnt   Score    Error  Units
-     * CollectorsVsStreamToList.viaCollectors                         thrpt   20  17.321 ±  0.583  ops/s
-     * CollectorsVsStreamToList.viaStream                             thrpt   20  23.879 ±  1.682  ops/s
-     * CollectorsVsStreamToList.viaCollectors                          avgt   20   0.057 ±  0.002   s/op
-     * CollectorsVsStreamToList.viaStream                              avgt   20   0.040 ±  0.001   s/op
-     * CollectorsVsStreamToList.viaCollectors                        sample  380   0.054 ±  0.001   s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.00    sample        0.051            s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.50    sample        0.054            s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.90    sample        0.058            s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.95    sample        0.058            s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.99    sample        0.062            s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.999   sample        0.068            s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.9999  sample        0.068            s/op
-     * CollectorsVsStreamToList.viaCollectors:viaCollectors·p1.00    sample        0.068            s/op
-     * CollectorsVsStreamToList.viaStream                            sample  525   0.039 ±  0.001   s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p0.00            sample        0.037            s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p0.50            sample        0.038            s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p0.90            sample        0.040            s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p0.95            sample        0.042            s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p0.99            sample        0.050            s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p0.999           sample        0.051            s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p0.9999          sample        0.051            s/op
-     * CollectorsVsStreamToList.viaStream:viaStream·p1.00            sample        0.051            s/op
-     * CollectorsVsStreamToList.viaCollectors                            ss   20   0.060 ±  0.007   s/op
-     * CollectorsVsStreamToList.viaStream                                ss   20   0.043 ±  0.006   s/op
-     */
 }
+/*
+ * Benchmark                                                       Mode  Cnt   Score    Error  Units
+ * CollectorsVsStreamToList.viaCollectors                         thrpt   20  17.321 ±  0.583  ops/s
+ * CollectorsVsStreamToList.viaStream                             thrpt   20  23.879 ±  1.682  ops/s
+ * CollectorsVsStreamToList.viaCollectors                          avgt   20   0.057 ±  0.002   s/op
+ * CollectorsVsStreamToList.viaStream                              avgt   20   0.040 ±  0.001   s/op
+ * CollectorsVsStreamToList.viaCollectors                        sample  380   0.054 ±  0.001   s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.00    sample        0.051            s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.50    sample        0.054            s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.90    sample        0.058            s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.95    sample        0.058            s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.99    sample        0.062            s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.999   sample        0.068            s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p0.9999  sample        0.068            s/op
+ * CollectorsVsStreamToList.viaCollectors:viaCollectors·p1.00    sample        0.068            s/op
+ * CollectorsVsStreamToList.viaStream                            sample  525   0.039 ±  0.001   s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p0.00            sample        0.037            s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p0.50            sample        0.038            s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p0.90            sample        0.040            s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p0.95            sample        0.042            s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p0.99            sample        0.050            s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p0.999           sample        0.051            s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p0.9999          sample        0.051            s/op
+ * CollectorsVsStreamToList.viaStream:viaStream·p1.00            sample        0.051            s/op
+ * CollectorsVsStreamToList.viaCollectors                            ss   20   0.060 ±  0.007   s/op
+ * CollectorsVsStreamToList.viaStream                                ss   20   0.043 ±  0.006   s/op
+ */
